fb2abc979668f65571c0526d2d726a86580849bc
Numan Ahmed <mdnmnahmed@gmail.com>
2021-05-21 01:27:12 +0530

    Typography

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import BoxComponent from "./Components/BasicComponents/BoxComponent";
+import ContainerComponent from "./Components/BasicComponents/ContainerComponent";
+import TypographyComponent from "./Components/BasicComponents/TypographyComponent";
 
 const App = () => {
     return (
         <>
-            <BoxComponent />
+            {/* <BoxComponent /> */}
+            {/* <ContainerComponent /> */}
+            <TypographyComponent />
         </>
     );
 };
---
+++ src/Components/BasicComponents/TypographyComponent.js
@@ -0,0 +1,65 @@
+import { Typography } from '@material-ui/core';
+
+const TypographyComponent = () => {
+    return (
+        <>
+            <Typography variant="h3">Hii, This is Numan</Typography>
+            <Typography variant="h5">Hii, This is Numan</Typography>
+
+            <br />
+
+            <Typography variant="h4" align="center" color="secondary">
+                Hii, This is Numan
+            </Typography>
+
+            <Typography variant="h4" align="right">
+                Hii, This is Numan
+            </Typography>
+
+            <Typography variant="h4" display="inline">
+                Hii, This is Numan
+            </Typography>
+
+
+            <Typography variant="body1">
+                Hey, this is Numan, a Full Stack Developer from INDIA
+                Hey, this is Numan, a Full Stack Developer from INDIA
+                Hey, this is Numan, a Full Stack Developer from INDIA
+                Hey, this is Numan, a Full Stack Developer from INDIA
+            </Typography>
+
+            <Typography variant="body2">
+                Hey, this is Numan, a Full Stack Developer from INDIA
+                Hey, this is Numan, a Full Stack Developer from INDIA
+                Hey, this is Numan, a Full Stack Developer from INDIA
+                Hey, this is Numan, a Full Stack Developer from INDIA
+            </Typography>
+
+            <Typography variant="subtitle1">
+                Hey, this is Numan, a Full Stack Developer from INDIA
+                Hey, this is Numan, a Full Stack Developer from INDIA
+                Hey, this is Numan, a Full Stack Developer from INDIA
+                Hey, this is Numan, a Full Stack Developer from INDIA
+            </Typography>
+
+            <Typography variant="subtitle1" align="justify" color="secondary"
+                gutterBottom
+            >
+                Hey, this is Numan, a Full Stack Developer from INDIA
+                Hey, this is Numan, a Full Stack Developer from INDIA
+                Hey, this is Numan, a Full Stack Developer from INDIA
+                Hey, this is Numan, a Full Stack Developer from INDIA
+            </Typography>
+
+            <Typography variant="subtitle1" color="primary" paragraph>
+                Hey, this is Numan, a Full Stack Developer from INDIA
+                Hey, this is Numan, a Full Stack Developer from INDIA
+                Hey, this is Numan, a Full Stack Developer from INDIA
+                Hey, this is Numan, a Full Stack Developer from INDIA
+            </Typography>
+
+        </>
+    );
+}
+
+export default TypographyComponent;
